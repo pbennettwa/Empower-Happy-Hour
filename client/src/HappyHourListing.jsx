42f9461f04ae65feca6153e636607d2589b7ca4b
Patrick Bennett <pbennettwa@gmail.com>
2020-01-13 19:31:13 -0800

    add minor listing data

--- client/src/HappyHourListing.jsx
+++ client/src/HappyHourListing.jsx
@@ -5,7 +5,8 @@ const HappyHourListing = ({ bar }) => {
   return (
     <div className='listing'>
       <h4 className='name'>{bar.venue.name}</h4>
-      <h6 className='type'> - {bar.venue.categories[0].name}</h6>
+      <h5 className='type'> - {bar.venue.categories[0].name}</h5>
+      <h6 className='location'>{bar.venue.location.formattedAddress[0]}</h6>
       <p>{(getDistance({ latitude:47.5991664, longitude:-122.3331533 }, { latitude:bar.venue.location.lat, longitude:bar.venue.location.lng })/1609).toFixed(2)} miles away</p>
     </div>
   )
